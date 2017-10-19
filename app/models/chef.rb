b2c5388a0e692c73dae170de78f5fb6902876a6d
ashwinder bhinder <ashwinderbhinder24@gmail.com>
2017-10-18 22:00:14 -0400

    create chef and recipe association

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,2 +1,6 @@
 class Chef < ApplicationRecord
+    has_many :recipes
+    before_save { self.email = email.downcase }
+    validates :chefname, presence: true, length: {minimum: 2, maximum: 40}
+    validates :email, presence: true, length: {maximum: 105}, uniqueness: { case_sensitive: false }, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
 end
